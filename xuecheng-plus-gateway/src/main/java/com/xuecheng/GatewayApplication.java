9b1ec800b6c2dd1bc8fb170268335241b7b623b8
YZC <123456@qq.com>
2023-08-01 01:12:26 +0800

    day05

--- xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -3,9 +3,7 @@ package com.xuecheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 网关启动类
- */
+
 @SpringBootApplication
 public class GatewayApplication {
 
